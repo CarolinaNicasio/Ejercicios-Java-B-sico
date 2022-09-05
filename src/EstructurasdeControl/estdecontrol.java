c551ae359f6709b91becd186ba53e2b96e29c612
Carolina <cfnicasio141@gmail.com>
2022-09-05 17:32:14 -0300

    Ejercicios de Java Básico

---
+++ src/EstructurasdeControl/estdecontrol.java
@@ -0,0 +1,13 @@
+package EstructurasdeControl;
+
+public class estdecontrol {
+    public static void main(String[] args) {
+
+        String [] capitales = {"CABA", "Berlín", "Washington", "Quito", "Bogóta", "Santiago de Chile"};
+
+        for (int i = 2; i < capitales.length; i++) {
+            System.out.println(capitales[i]);
+
+        }
+    }
+}
